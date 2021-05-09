4beba968afdd01e8944be89fa0ddfe6ccc7a2435
Phan Nhật Huy <72343262+pnhathuy07@users.noreply.github.com>
2021-05-09 10:17:22 +0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.DataFrame({'a': [1, 2, 1, 5,6,7,7,9,9,8,9], 'b': [1, 2, 3, 5,6,5,3,3,5,5,5]})
-
-print(df)
-
-df['count'] = df.groupby('a')['a'].transform(pd.Series.count())
-df.sort('count', ascending=False)
-
-print(df)
